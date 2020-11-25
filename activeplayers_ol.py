0b71d11fc9339c550e05f99f5260244fe4949fb6
keimurakami <k.m.1996.0621@gmail.com>
2020-11-25 12:33:59 +0900

    change name of variable

--- activeplayers_ol.py
+++ activeplayers_ol.py
@@ -6,7 +6,7 @@ sys.path.append("./")
 import ownersleague_cards_list
 import time
 
-teams = {
+npb_teams = {
     "オリックス・バファローズ" : "http://npb.jp/bis/teams/rst_b.html",
     "埼玉西武ライオンズ" : "http://npb.jp/bis/teams/rst_l.html",
     "千葉ロッテマリーンズ" : "http://npb.jp/bis/teams/rst_m.html",
@@ -21,20 +21,21 @@ teams = {
     "読売ジャイアンツ" : "http://npb.jp/bis/teams/rst_g.html"
 }
 
-count_all = 0
-for team_name, team_url in teams.items():
-    print(team_name)
-    r = requests.get(team_url)
+npb_players = []
+activeplayers_count_all = 0
+for npb_team_name, npb_team_url in npb_teams.items():
+    print(npb_team_name)
+    r = requests.get(npb_team_url)
     soup = BeautifulSoup(r.content, 'html.parser')
-    players = soup.find_all(href=re.compile('/bis/players/'))
-    count = 0
-    for player in players:
-        modified_player = (player.getText()).replace('　', '')
-        if modified_player in ownersleague_cards_list.cardlist or modified_player == '金子弌大':
-            print(modified_player)
-            count_all += 1
-            count += 1
-    print('オーナーズリーグに収録された現役選手：' + str(count) + '人\n')
+    npb_players = (soup.find_all(href=re.compile('/bis/players/')))
+    activeplayers_count_team = 0
+    for npb_player in npb_players:
+        npb_player_modified = (npb_player.getText()).replace('　', '')
+        if npb_player_modified in ownersleague_cards_list.cardlist or npb_player_modified == '金子弌大':
+            print(npb_player_modified)
+            activeplayers_count_all += 1
+            activeplayers_count_team += 1
+    print('オーナーズリーグに収録された現役選手：' + str(activeplayers_count_team) + '人\n')
     time.sleep(1)
-print('オーナーズリーグに収録された全チームの現役選手：' + str(count_all) + '人\n')
+print('オーナーズリーグに収録された全チームの現役選手：' + str(activeplayers_count_all) + '人\n')
 print('同じ登録名の別の選手が含まれることがあります。')
